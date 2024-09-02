70a1e93a95df4c6cee7feb7435507afed7718cfb
Ritesh Sharma <riteshbbn74@gmail.com>
2024-09-02 12:40:16 +0530

    add protected routes

--- src/Layouts/Dashboard.tsx
+++ src/Layouts/Dashboard.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const Dashboard = () => {
+  const { user } = useAuthStore();
+  console.log(user);
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />;
+  }
   return (
     <>
       <h1>Dashboard</h1>
--- src/Layouts/Unauthenticated.tsx
+++ src/Layouts/Unauthenticated.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const Unauthenticated = () => {
+  const { user } = useAuthStore();
+  if (user !== null) {
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <>
       <h1>Unauthenticated</h1>
