e0519354e8754d0e1483ea63dbb9e6286206091c
Ritesh Sharma <riteshbbn74@gmail.com>
2024-09-02 13:06:37 +0530

    persist auth on refresh

--- src/router.tsx
+++ src/router.tsx
@@ -3,16 +3,23 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/login/login";
 import Dashboard from "./Layouts/Dashboard";
 import Unauthenticated from "./Layouts/Unauthenticated";
+import Root from "./Layouts/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    children: [{ path: "", element: <HomePage /> }],
-  },
-  {
-    path: "/auth",
-    element: <Unauthenticated />,
-    children: [{ path: "login", element: <LoginPage /> }],
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+        children: [{ path: "", element: <HomePage /> }],
+      },
+      {
+        path: "/auth",
+        element: <Unauthenticated />,
+        children: [{ path: "login", element: <LoginPage /> }],
+      },
+    ],
   },
 ]);
