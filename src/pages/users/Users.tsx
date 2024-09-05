65ed0c088c583921ac8214d05db76a651783a24b
Ritesh Sharma <riteshbbn74@gmail.com>
2024-09-05 05:52:28 +0530

    add user table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,53 @@
-import { Breadcrumb } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Breadcrumb, Table } from "antd";
 import { Link } from "react-router-dom";
+import { getUsers } from "../../http/api";
+import { render } from "@testing-library/react";
+import { User } from "../../types";
+
+const columns = [
+  {
+    title: "Id",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 
 const Users = () => {
+  const {
+    data: userData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => {
+      return getUsers().then((res) => res.data);
+    },
+  });
+
   return (
     <>
       <Breadcrumb
@@ -11,6 +57,14 @@ const Users = () => {
           { title: <Link to="/users">Users</Link> },
         ]}
       />
+      {isLoading && <h1>Loading....</h1>}
+      {isError && <h1>{error.message}</h1>}
+      <Table
+        style={{ paddingTop: "1rem" }}
+        dataSource={userData}
+        columns={columns}
+      />
+      ;
     </>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export type Credentials = {
   email: string;
   password: string;
 };
+
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+};
