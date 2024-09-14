80139dd7bfd42ddf4a206f3ceb474f93efd8d0bd
Ritesh Sharma <riteshbbn74@gmail.com>
2024-09-14 11:58:31 +0530

    on hard reload back to homepage fix

--- src/Layouts/Dashboard.tsx
+++ src/Layouts/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store";
 import {
   Avatar,
@@ -67,6 +67,7 @@ const getMenuItems = (role: string) => {
 };
 
 const Dashboard = () => {
+  const location = useLocation();
   const { user, logout: logoutFromStore } = useAuthStore();
 
   const { mutate: logoutMutate } = useMutation({
@@ -83,7 +84,12 @@ const Dashboard = () => {
   } = theme.useToken();
   console.log(user);
   if (user === null) {
-    return <Navigate to="/auth/login" replace={true} />;
+    return (
+      <Navigate
+        to={`/auth/login?returnTo=${location.pathname}`}
+        replace={true}
+      />
+    );
   }
   const items = getMenuItems(user?.role);
   return (
